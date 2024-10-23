c1880059ff367054d41d57876635215c27201ada
ashura <138534881+ppooggeerrss@users.noreply.github.com>
2024-10-22 22:01:33 -0300

    aula 22/10

---
+++ app.js
@@ -0,0 +1,3 @@
+function criaCartao (item, pergunta, resposta) {
+    console.log(item, pergunta,resposta)
+}
---
+++ styles/MissaoVisaoValores.js
@@ -0,0 +1,17 @@
+criaCartao (
+    `Missão`,
+    `saiba qual e a missão de nossa empresa!`
+    `nossa missão é criar jogos e aplicativos`
+)
+
+criaCartao(
+    `Visão`,
+    `saiba qual é a visão de nosa empresa`
+    `nossa visão é ser a melhor empresa criadora de jogos`
+)
+
+criaCartao(
+    `valores`,
+    `quais são os valores de nossa empresa!`
+    `nosos valores são qualidade,respeito com os clientes,alta qualidade nos jogos e apps`
+)
