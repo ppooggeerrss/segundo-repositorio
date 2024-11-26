e31092dc77adaf6ea7d68f929ae12b519b20a63a
ashura <138534881+ppooggeerrss@users.noreply.github.com>
2024-11-26 19:31:37 -0300

    alt 2

--- MissaoVisaoValores.js
+++ MissaoVisaoValores.js
@@ -1,17 +1,17 @@
 criaCartao (
     'Missão',
     'saiba qual e a missão de nossa empresa!',
-    'nossa missão é criar jogos e aplicativos'
+    'nossa missão é ensinar a arte da programação para todos(a)'
 )
 
 criaCartao(
     'Visão',
     'saiba qual é a visão de nosa empresa',
-    'nossa visão é ser a melhor empresa criadora de jogos'
+    'nossa visão é fazer novos programadores de jogos ou apps'
 )
 
 criaCartao(
     'valores',
     'quais são os valores de nossa empresa!',
-    'nosos valores são qualidade,respeito com os clientes,alta qualidade nos jogos e apps'
+    'nosos valores são qualidade,respeito com os clientes,alta qualidade com os ensinamentos de programação'
 )
